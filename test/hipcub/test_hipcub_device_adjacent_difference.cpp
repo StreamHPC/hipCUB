f3efd42a5faddceb291223b82c5891cd9f8534ae
Saiyang Zhang <saiyang@streamhpc.com>
2024-11-07 09:26:03 +0000

    Formatting

--- test/hipcub/test_hipcub_device_adjacent_difference.cpp
+++ test/hipcub/test_hipcub_device_adjacent_difference.cpp
@@ -585,8 +585,8 @@ TYPED_TEST(HipcubDeviceAdjacentDifferenceLargeTests, LargeIndicesAndOpOnce)
     HIP_CHECK(hipSetDevice(device_id));
 
     using T                                     = size_t;
-    static constexpr bool left                  = TestFixture::left;
-    static constexpr bool copy                  = TestFixture::copy;
+    static constexpr bool         left          = TestFixture::left;
+    static constexpr bool         copy          = TestFixture::copy;
     static constexpr unsigned int sampling_rate = 10000;
     using OutputIterator                        = check_output_iterator<sampling_rate>;
     using flag_type                             = OutputIterator::flag_type;
